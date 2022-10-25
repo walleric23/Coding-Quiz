7f7443f4dcf00bf694fc2fcfd89e3778e1df2e82
walleric23 <walleric23@gmail.com>
2022-10-24 22:05:54 -0700

    finished code/comments

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
+// listener for button to begin quiz
 document.querySelector(".startQuiz").addEventListener("click", function () {
+  // once quiz starts, start area display turns to hidden
   document.querySelector(".start-area").style.display = "none";
+  // quiz questions and timer display in block
   document.querySelector(".quiz-area").style.display = "block";
   displayQuestions();
   startTimer();
 });
+// quiz variables for timer and questions
 var time = 60;
 var timer;
 var questionIndex = 0;
+// array of questions 1-3
 var questions = [
   {
     question: "What is HTML?",
+    // possible answers
     choices: [
       "Hypertext Markup Language",
       "Cascading Style Sheet",
       "Computer",
       "sup",
     ],
+    // correct answers
     answer: "Hypertext Markup Language",
   },
 
@@ -36,14 +43,17 @@ var questions = [
     answer: "JavaScript Object Notation",
   },
 ];
+// function for cycling through questions until all have been answered
 function displayQuestions() {
   if (questionIndex === questions.length) {
+    // once all questions are answered timer stops and scores display
     clearInterval(timer);
     document.querySelector(".quiz-area").style.display = "none";
     document.querySelector(".scores").style.display = "block";
     document.querySelector(".highscores").innerText = "score " + time;
     return;
   }
+  // edits questions on screen
   document.querySelector(".questionText").innerText =
     questions[questionIndex].question;
   document.querySelector(".answer1").innerText =
@@ -55,6 +65,7 @@ function displayQuestions() {
   document.querySelector(".answer4").innerText =
     questions[questionIndex].choices[3];
 }
+// if user selects wrong answer 3 seconds are subracted from the timer
 document
   .querySelector(".quiz-area")
   .addEventListener("click", function (event) {
@@ -72,6 +83,7 @@ document
       displayQuestions();
     }
   });
+// function and interval for timer
 function startTimer() {
   timer = setInterval(function () {
     if (time > 0) {
@@ -80,6 +92,7 @@ function startTimer() {
     }
   }, 1000);
 }
+// event listeners for submit button and initials.
 document.querySelector(".submit").addEventListener("click", function () {
   var initials = document.querySelector(".initials").value;
   var highscores = JSON.parse(localStorage.getItem("scores")) || [];
@@ -87,6 +100,7 @@ document.querySelector(".submit").addEventListener("click", function () {
   localStorage.setItem("scores", JSON.stringify(highscores));
   window.location.reload();
 });
+// edits highscores on page.
 var highscores = JSON.parse(localStorage.getItem("scores")) || [];
 for (var i = 0; i < highscores.length; i++) {
   var score = document.createElement("p");
